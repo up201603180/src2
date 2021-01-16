a4cc129dc06e8fd21cff9f1b9d97a4fc626e5dbd
tiago <up201603180@fe.up.pt>
2021-01-16 12:24:34 +0000

    Node initialization

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,6 @@
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
@@ -9,23 +12,21 @@ public class Node {
 
     // Variables for the Multicast Communication
     private static final int    MULTICAST_PORT = 1234;
-    private static final String MULTICAST_ADDRESS = "228.5.6.7";
     Receiver receiver;
 
     // Variables kept during election
     private int src_i; // Computation index of the diffusing computation
     private int delta_i; // 0 if node has sent its pending ACK message to its parent
-    private Boolean in_election; // 1 if the node is electing a leader
+    private boolean in_election; // 1 if the node is electing a leader
     private int nodeID; // this node id
-    private int node_parent; // id of the parent node
-    private int node_leader; // id of the leader node (-1 when a node starts its execution)
-    private ArrayList<Node> neighbour_nodes = new ArrayList<>();
+    private int nodeParent; // id of the parent node
+    private int nodeLeader; // id of the leader node (-1 when a node starts its execution)
+    private ArrayList<NodeInfo> neighbourNodes = new ArrayList<>();
     private ArrayList<Node> nodes_left_to_ack = new ArrayList<>();
-    private Map<Integer,Integer> nodeInfoList = new HashMap<>();
 
     // Variables for node coordinates and value
-    private int x_coord, y_coord, value;
-    private boolean hasLeader = false; //Variable to indicate if node has leader
+    private int xCoord, yCoord, value;
+    private boolean hasLeader = false; //Variable to indicate if node has leader(delta minusculo)
     private int lid_i; // Current leader
 
     /*
@@ -35,16 +36,57 @@ public class Node {
     public Node ( int nodeID, int x_coord, int y_coord, int value ) {
 
         this.nodeID = nodeID;
-        this.x_coord = x_coord;
-        this.y_coord = y_coord;
+        this.xCoord = x_coord;
+        this.yCoord = y_coord;
         this.value = value;
 
-        this.receiver = new Receiver( nodeID, MULTICAST_ADDRESS, MULTICAST_PORT );
+        //this.receiver = new Receiver( nodeID, MULTICAST_ADDRESS, MULTICAST_PORT );
     }
 
     /*
      * Methods
      */
+
+    public void initializeNode(Node node) throws Exception{
+        NodeInfo node1 = new NodeInfo(1, 3, 0, 2);
+        NodeInfo node2 = new NodeInfo(2, 2, 2, 1);
+        NodeInfo node3 = new NodeInfo(3, 5, 2, 3);
+        NodeInfo node4 = new NodeInfo(4, 10, 3, 3);
+        NodeInfo node5 = new NodeInfo(5, 7, 3, 1);
+
+        //Tree initialization
+        switch(node.nodeID) {
+            case 1:
+                node.nodeParent = 1;
+                node.neighbourNodes.add(node2);
+                node.neighbourNodes.add(node3);
+                InetAddress group1 = InetAddress.getByName("230.0.0.1");
+                MulticastSocket receiveSocket = new MulticastSocket(MULTICAST_PORT); //Receiver socket
+                DatagramSocket transmitSocket = new DatagramSocket();
+            case 2:
+                node.nodeParent = 1;
+                node.neighbourNodes.add(node1);
+                node.neighbourNodes.add(node3);
+                node.neighbourNodes.add(node5);
+            case 3:
+                node.nodeParent = 1;
+                node.neighbourNodes.add(node1);
+                node.neighbourNodes.add(node2);
+                node.neighbourNodes.add(node4);
+                break;
+            case 4:
+                node.nodeParent = 3;
+                node.neighbourNodes.add(node3);
+                node.neighbourNodes.add(node5);
+                break;
+            case 5:
+                node.nodeParent = 2;
+                node.neighbourNodes.add(node2);
+                node.neighbourNodes.add(node4);
+                break;
+        }
+    }
+
 
     //Getters
     public int getNodeID() {
@@ -52,11 +94,11 @@ public class Node {
     }
 
     public int getX_coord() {
-        return x_coord;
+        return xCoord;
     }
 
     public int getY_coord() {
-        return y_coord;
+        return yCoord;
     }
 
     public int getValue() {
@@ -70,16 +112,17 @@ public class Node {
         }
         Node node = new Node( Integer.parseInt(args[0]), Integer.parseInt(args[1]),Integer.parseInt(args[2]), Integer.parseInt(args[3]) );
         System.out.println( "ID = " + node.getNodeID() + "\nx = " + node.getX_coord() + "\ny = " + node.getY_coord() + "\nvalue = " + node.getValue() );
+
         //Declaração do objecto para o thread receptor
-        Receiver receiver = new Receiver(Integer.parseInt(args[0]), MULTICAST_ADDRESS, MULTICAST_PORT);
+        //Receiver receiver = new Receiver(Integer.parseInt(args[0]), MULTICAST_ADDRESS, MULTICAST_PORT);
         //Declaração do objecto para o thread transmissor
         Transmitter transmitter = new Transmitter(Integer.parseInt(args[0]), Integer.parseInt(args[3]));
         //Declaração dos threads
-        Thread threadReceiver = new Thread( receiver );
-        threadReceiver.start();
+        //Thread threadReceiver = new Thread( receiver );
+        //threadReceiver.start();
         Thread threadTransmitter = new Thread( transmitter);
         threadTransmitter.start();
-        TimeUnit.MILLISECONDS.sleep( 10 );
+        //Election algorithm
 
     }
 
--- src/NodeInfo.java
+++ src/NodeInfo.java
@@ -1,9 +1,14 @@
-final class NodeInfo {
-    public final int nodeId;
-    public final int value;
 
-    public NodeInfo(int nodeId, int value) {
-        this.nodeId = nodeId;
+public class NodeInfo {
+    public int nodeID;
+    public int value;
+    public int coordsX;
+    public int coordsY;
+
+    public NodeInfo(int nodeID, int value, int coordsX, int coordsY) {
+        this.nodeID = nodeID;
         this.value = value;
+        this.coordsX = coordsX;
+        this.coordsY = coordsY;
     }
 }
